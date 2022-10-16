340791e46764e02e8de6c80846351e935851db93
Dario Velazquez <v.dario.developer@gmail.com>
2022-10-16 19:34:51 +0000

    Add random colors

---
+++ assets/JS/script.js
@@ -0,0 +1,6 @@
+var randomColor1 = '#'+Math.floor(Math.random()*16777215).toString(16);
+document.getElementById('color1').style.backgroundColor = randomColor1 ;
+
+var randomColor2 = '#'+Math.floor(Math.random()*16777215).toString(16);
+document.getElementById('color2').style.backgroundColor = randomColor2 ;
+
