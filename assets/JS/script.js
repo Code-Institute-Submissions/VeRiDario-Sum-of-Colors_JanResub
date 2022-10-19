d99df8e257f4ec4c4d071b763118cf2a76c98428
Dario Velazquez <v.dario.developer@gmail.com>
2022-10-19 23:42:00 +0000

    Add Javascript

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,39 +10,42 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
-    document.getElementById("answer").value = "";
-    document.getElementById("answer").focus();
-
 let randomColor1 = '#'+Math.floor(Math.random()*16777215).toString(16);
 document.getElementById('color1').style.backgroundColor = randomColor1 ;
 
 let randomColor2 = '#'+Math.floor(Math.random()*16777215).toString(16);
 document.getElementById('color2').style.backgroundColor = randomColor2 ;
- 
-function checktAnswer() {
-        let userAnswer = parseInt(document.getElementById("answer").value);
-        let calculatedAnswer = correctAnswer();
-        let isCorrect = userAnswer === calculatedAnswer[0];
-    
-        if (isCorrect) {
-            alert("Right answer!");
-            incrementScore();
-        } else {
-            alert(`Wrong answer!`);
-            incrementWrongAnswer();
-        }
-    
-        runGame(calculatedAnswer[1]);   
-}
 
-var correctAnswer = mix(randomColor1, randomColor2);
+var correctAnswer = blendColors(randomColor1, randomColor2,0.5);
 
 function incrementScore() {
-    let oldScore = parseInt(document.getElementById("correct").innerText);
+    let oldScore = parseInt(document.getElementById("correct").innerText);``````````
     document.getElementById("correct").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer() {
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
+}
+
+function submitAnswer()
+{
+    var answer = document.getElementById('answer').value;
+    if (answer == correctAnswer) {
+        alert("Right answer!");
+        incrementScore();
+    } else {
+        alert(`Wrong answer!`);
+        incrementWrongAnswer();
+    }
+}
+
+// blend two hex colors together by an amount
+function blendColors(colorA, colorB, amount) {
+  const [rA, gA, bA] = colorA.match(/\w\w/g).map((c) => parseInt(c, 16));
+  const [rB, gB, bB] = colorB.match(/\w\w/g).map((c) => parseInt(c, 16));
+  const r = Math.round(rA + (rB - rA) * amount).toString(16).padStart(2, '0');
+  const g = Math.round(gA + (gB - gA) * amount).toString(16).padStart(2, '0');
+  const b = Math.round(bA + (bB - bA) * amount).toString(16).padStart(2, '0');
+  return '#' + r + g + b;
 }
